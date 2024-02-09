bc3134c3024dde5af266e2b371d59bf4485049e7
Sebi <Sebi@DESKTOP-IN2IS25>
2024-02-09 13:06:24 +0200

    Add project files.

---
+++ Aplicatia 7/Form1.Designer.cs
@@ -0,0 +1,359 @@
+﻿namespace Aplicatia_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.OFF = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.Suma = new System.Windows.Forms.Button();
+            this.Nr7 = new System.Windows.Forms.Button();
+            this.Nr4 = new System.Windows.Forms.Button();
+            this.Nr1 = new System.Windows.Forms.Button();
+            this.Nr8 = new System.Windows.Forms.Button();
+            this.Nr9 = new System.Windows.Forms.Button();
+            this.Scadere = new System.Windows.Forms.Button();
+            this.Nr5 = new System.Windows.Forms.Button();
+            this.Nr6 = new System.Windows.Forms.Button();
+            this.Inmultire = new System.Windows.Forms.Button();
+            this.nr2 = new System.Windows.Forms.Button();
+            this.Egal = new System.Windows.Forms.Button();
+            this.Nr3 = new System.Windows.Forms.Button();
+            this.Impartire = new System.Windows.Forms.Button();
+            this.Nr0 = new System.Windows.Forms.Button();
+            this.Virgula = new System.Windows.Forms.Button();
+            this.ON = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.textBox1.Location = new System.Drawing.Point(12, 21);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(229, 29);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // OFF
+            // 
+            this.OFF.BackColor = System.Drawing.Color.Red;
+            this.OFF.Location = new System.Drawing.Point(12, 75);
+            this.OFF.Name = "OFF";
+            this.OFF.Size = new System.Drawing.Size(53, 31);
+            this.OFF.TabIndex = 1;
+            this.OFF.Text = "OFF";
+            this.OFF.UseVisualStyleBackColor = false;
+            this.OFF.Click += new System.EventHandler(this.OFF_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Yellow;
+            this.button2.Location = new System.Drawing.Point(71, 75);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(53, 31);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "<--";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.BackColor = System.Drawing.Color.Yellow;
+            this.Clear.Location = new System.Drawing.Point(130, 75);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(53, 31);
+            this.Clear.TabIndex = 3;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = false;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // Suma
+            // 
+            this.Suma.BackColor = System.Drawing.Color.Yellow;
+            this.Suma.Location = new System.Drawing.Point(188, 75);
+            this.Suma.Name = "Suma";
+            this.Suma.Size = new System.Drawing.Size(53, 31);
+            this.Suma.TabIndex = 4;
+            this.Suma.Text = "+";
+            this.Suma.UseVisualStyleBackColor = false;
+            this.Suma.Click += new System.EventHandler(this.Suma_Click);
+            // 
+            // Nr7
+            // 
+            this.Nr7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Nr7.Location = new System.Drawing.Point(12, 112);
+            this.Nr7.Name = "Nr7";
+            this.Nr7.Size = new System.Drawing.Size(53, 31);
+            this.Nr7.TabIndex = 5;
+            this.Nr7.Text = "7";
+            this.Nr7.UseVisualStyleBackColor = false;
+            this.Nr7.Click += new System.EventHandler(this.Nr7_Click);
+            // 
+            // Nr4
+            // 
+            this.Nr4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Nr4.Location = new System.Drawing.Point(12, 149);
+            this.Nr4.Name = "Nr4";
+            this.Nr4.Size = new System.Drawing.Size(53, 31);
+            this.Nr4.TabIndex = 6;
+            this.Nr4.Text = "4";
+            this.Nr4.UseVisualStyleBackColor = false;
+            this.Nr4.Click += new System.EventHandler(this.Nr4_Click);
+            // 
+            // Nr1
+            // 
+            this.Nr1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Nr1.Location = new System.Drawing.Point(12, 186);
+            this.Nr1.Name = "Nr1";
+            this.Nr1.Size = new System.Drawing.Size(53, 31);
+            this.Nr1.TabIndex = 7;
+            this.Nr1.Text = "1";
+            this.Nr1.UseVisualStyleBackColor = false;
+            this.Nr1.Click += new System.EventHandler(this.Nr1_Click);
+            // 
+            // Nr8
+            // 
+            this.Nr8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Nr8.Location = new System.Drawing.Point(72, 112);
+            this.Nr8.Name = "Nr8";
+            this.Nr8.Size = new System.Drawing.Size(53, 31);
+            this.Nr8.TabIndex = 8;
+            this.Nr8.Text = "8";
+            this.Nr8.UseVisualStyleBackColor = false;
+            this.Nr8.Click += new System.EventHandler(this.Nr8_Click);
+            // 
+            // Nr9
+            // 
+            this.Nr9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Nr9.Location = new System.Drawing.Point(131, 112);
+            this.Nr9.Name = "Nr9";
+            this.Nr9.Size = new System.Drawing.Size(53, 31);
+            this.Nr9.TabIndex = 9;
+            this.Nr9.Text = "9";
+            this.Nr9.UseVisualStyleBackColor = false;
+            this.Nr9.Click += new System.EventHandler(this.Nr9_Click);
+            // 
+            // Scadere
+            // 
+            this.Scadere.BackColor = System.Drawing.Color.Yellow;
+            this.Scadere.Location = new System.Drawing.Point(188, 112);
+            this.Scadere.Name = "Scadere";
+            this.Scadere.Size = new System.Drawing.Size(53, 31);
+            this.Scadere.TabIndex = 10;
+            this.Scadere.Text = "-";
+            this.Scadere.UseVisualStyleBackColor = false;
+            this.Scadere.Click += new System.EventHandler(this.Scadere_Click);
+            // 
+            // Nr5
+            // 
+            this.Nr5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Nr5.Location = new System.Drawing.Point(71, 149);
+            this.Nr5.Name = "Nr5";
+            this.Nr5.Size = new System.Drawing.Size(53, 31);
+            this.Nr5.TabIndex = 11;
+            this.Nr5.Text = "5";
+            this.Nr5.UseVisualStyleBackColor = false;
+            this.Nr5.Click += new System.EventHandler(this.Nr5_Click);
+            // 
+            // Nr6
+            // 
+            this.Nr6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Nr6.Location = new System.Drawing.Point(130, 149);
+            this.Nr6.Name = "Nr6";
+            this.Nr6.Size = new System.Drawing.Size(53, 31);
+            this.Nr6.TabIndex = 12;
+            this.Nr6.Text = "6";
+            this.Nr6.UseVisualStyleBackColor = false;
+            this.Nr6.Click += new System.EventHandler(this.Nr6_Click);
+            // 
+            // Inmultire
+            // 
+            this.Inmultire.BackColor = System.Drawing.Color.Yellow;
+            this.Inmultire.Location = new System.Drawing.Point(189, 149);
+            this.Inmultire.Name = "Inmultire";
+            this.Inmultire.Size = new System.Drawing.Size(53, 31);
+            this.Inmultire.TabIndex = 13;
+            this.Inmultire.Text = "*";
+            this.Inmultire.UseVisualStyleBackColor = false;
+            this.Inmultire.Click += new System.EventHandler(this.Inmultire_Click);
+            // 
+            // nr2
+            // 
+            this.nr2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.nr2.Location = new System.Drawing.Point(71, 186);
+            this.nr2.Name = "nr2";
+            this.nr2.Size = new System.Drawing.Size(53, 31);
+            this.nr2.TabIndex = 14;
+            this.nr2.Text = "2";
+            this.nr2.UseVisualStyleBackColor = false;
+            this.nr2.Click += new System.EventHandler(this.nr2_Click);
+            // 
+            // Egal
+            // 
+            this.Egal.BackColor = System.Drawing.Color.Yellow;
+            this.Egal.Location = new System.Drawing.Point(130, 223);
+            this.Egal.Name = "Egal";
+            this.Egal.Size = new System.Drawing.Size(111, 31);
+            this.Egal.TabIndex = 15;
+            this.Egal.Text = "=";
+            this.Egal.UseVisualStyleBackColor = false;
+            this.Egal.Click += new System.EventHandler(this.Egal_Click);
+            // 
+            // Nr3
+            // 
+            this.Nr3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Nr3.Location = new System.Drawing.Point(131, 186);
+            this.Nr3.Name = "Nr3";
+            this.Nr3.Size = new System.Drawing.Size(53, 31);
+            this.Nr3.TabIndex = 16;
+            this.Nr3.Text = "3";
+            this.Nr3.UseVisualStyleBackColor = false;
+            this.Nr3.Click += new System.EventHandler(this.Nr3_Click);
+            // 
+            // Impartire
+            // 
+            this.Impartire.BackColor = System.Drawing.Color.Yellow;
+            this.Impartire.Location = new System.Drawing.Point(188, 186);
+            this.Impartire.Name = "Impartire";
+            this.Impartire.Size = new System.Drawing.Size(53, 31);
+            this.Impartire.TabIndex = 17;
+            this.Impartire.Text = "/";
+            this.Impartire.UseVisualStyleBackColor = false;
+            this.Impartire.Click += new System.EventHandler(this.Impartire_Click);
+            // 
+            // Nr0
+            // 
+            this.Nr0.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Nr0.Location = new System.Drawing.Point(12, 223);
+            this.Nr0.Name = "Nr0";
+            this.Nr0.Size = new System.Drawing.Size(53, 31);
+            this.Nr0.TabIndex = 18;
+            this.Nr0.Text = "0";
+            this.Nr0.UseVisualStyleBackColor = false;
+            this.Nr0.Click += new System.EventHandler(this.Nr0_Click);
+            // 
+            // Virgula
+            // 
+            this.Virgula.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Virgula.Location = new System.Drawing.Point(72, 223);
+            this.Virgula.Name = "Virgula";
+            this.Virgula.Size = new System.Drawing.Size(53, 31);
+            this.Virgula.TabIndex = 19;
+            this.Virgula.Text = ".";
+            this.Virgula.UseVisualStyleBackColor = false;
+            this.Virgula.Click += new System.EventHandler(this.Virgula_Click);
+            // 
+            // ON
+            // 
+            this.ON.BackColor = System.Drawing.Color.Lime;
+            this.ON.Location = new System.Drawing.Point(12, 75);
+            this.ON.Name = "ON";
+            this.ON.Size = new System.Drawing.Size(53, 31);
+            this.ON.TabIndex = 20;
+            this.ON.Text = "ON";
+            this.ON.UseVisualStyleBackColor = false;
+            this.ON.Click += new System.EventHandler(this.ON_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(253, 287);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Virgula);
+            this.Controls.Add(this.Nr0);
+            this.Controls.Add(this.Impartire);
+            this.Controls.Add(this.Nr3);
+            this.Controls.Add(this.Egal);
+            this.Controls.Add(this.nr2);
+            this.Controls.Add(this.Inmultire);
+            this.Controls.Add(this.Nr6);
+            this.Controls.Add(this.Nr5);
+            this.Controls.Add(this.Scadere);
+            this.Controls.Add(this.Nr9);
+            this.Controls.Add(this.Nr8);
+            this.Controls.Add(this.Nr1);
+            this.Controls.Add(this.Nr4);
+            this.Controls.Add(this.Nr7);
+            this.Controls.Add(this.Suma);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.OFF);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ON);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button OFF;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button Suma;
+        private System.Windows.Forms.Button Nr7;
+        private System.Windows.Forms.Button Nr4;
+        private System.Windows.Forms.Button Nr1;
+        private System.Windows.Forms.Button Nr8;
+        private System.Windows.Forms.Button Nr9;
+        private System.Windows.Forms.Button Scadere;
+        private System.Windows.Forms.Button Nr5;
+        private System.Windows.Forms.Button Nr6;
+        private System.Windows.Forms.Button Inmultire;
+        private System.Windows.Forms.Button nr2;
+        private System.Windows.Forms.Button Egal;
+        private System.Windows.Forms.Button Nr3;
+        private System.Windows.Forms.Button Impartire;
+        private System.Windows.Forms.Button Nr0;
+        private System.Windows.Forms.Button Virgula;
+        private System.Windows.Forms.Button ON;
+        private System.Windows.Forms.Label label1;
+
+    }
+}
+
---
+++ Aplicatia 7/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aplicatia_7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        float num, ans;
+        int count;
+
+        public void disable()
+        {
+            textBox1.Enabled = false;
+            ON.Show();
+            OFF.Hide();
+            Virgula.Enabled = false;
+            Nr0.Enabled = false;
+            Nr1.Enabled = false;
+            nr2.Enabled = false;
+            Nr3.Enabled = false;
+            Nr4.Enabled = false;
+            Nr5.Enabled = false;
+            Nr6.Enabled = false;
+            Nr7.Enabled = false;
+            Nr8.Enabled = false;
+            Nr9.Enabled = false;
+            Suma.Enabled = false;
+            Inmultire.Enabled = false;
+            Impartire.Enabled = false;
+            Scadere.Enabled = false;
+            Egal.Enabled = false;
+            Clear.Enabled = false;
+            button2.Enabled = false;
+        }
+        public void enable()
+        {
+            textBox1.Enabled = true;
+            OFF.Show();
+            ON.Hide();
+            Virgula.Enabled = true;
+            Nr0.Enabled = true;
+            Nr1.Enabled = true;
+            nr2.Enabled = true;
+            Nr3.Enabled = true;
+            Nr4.Enabled = true;
+            Nr5.Enabled = true;
+            Nr6.Enabled = true;
+            Nr7.Enabled = true;
+            Nr8.Enabled = true;
+            Nr9.Enabled = true;
+            Suma.Enabled = true;
+            Inmultire.Enabled = true;
+            Impartire.Enabled = true;
+            Scadere.Enabled = true;
+            Egal.Enabled = true;
+            Clear.Enabled = true;
+            button2.Enabled = true;
+        }
+
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Virgula_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + ".";
+            textBox1.ForeColor = Color.Red;
+        }
+
+        private void Nr0_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 0;
+            textBox1.ForeColor = Color.Red;
+        }
+
+        private void Nr1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 1;
+            textBox1.ForeColor = Color.Red;
+        }
+
+        private void nr2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 2;
+            textBox1.ForeColor = Color.Red;
+        }
+
+        private void Nr3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 3;
+            textBox1.ForeColor = Color.Red;
+        }
+
+        private void Nr4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 4;
+            textBox1.ForeColor = Color.Red;
+        }
+
+        private void Nr5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 5;
+            textBox1.ForeColor = Color.Red;
+        }
+
+        private void Nr6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 6;
+            textBox1.ForeColor = Color.Red;
+        }
+
+        private void Nr7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 7;
+            textBox1.ForeColor = Color.Red;
+        }
+
+        private void Nr8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 8;
+            textBox1.ForeColor = Color.Red;
+        }
+
+        private void Nr9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 9;
+            textBox1.ForeColor = Color.Red;
+        }
+
+        private void OFF_Click(object sender, EventArgs e)
+        {
+            disable();
+        }
+
+        private void ON_Click(object sender, EventArgs e)
+        {
+            enable();
+        }
+        public void compute()
+        {
+            switch (count)
+            {
+                case 1:
+                    ans = num + float.Parse(textBox1.Text);
+                    textBox1.Text = ans.ToString();
+                    break;
+
+                case 2:
+                    ans = num - float.Parse(textBox1.Text);
+                    textBox1.Text = ans.ToString();
+                    break;
+
+                case 3:
+                    ans = num * float.Parse(textBox1.Text);
+                    textBox1.Text = ans.ToString();
+                    break;
+
+                case 4:
+                    ans = num / float.Parse(textBox1.Text);
+                    textBox1.Text = ans.ToString();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private void Suma_Click(object sender, EventArgs e)
+        {
+            num = float.Parse(textBox1.Text);
+            textBox1.Clear();
+            textBox1.Focus();
+            count = 1;
+            label1.Text = num.ToString() + "+";
+        }
+
+        private void Scadere_Click(object sender, EventArgs e)
+        {
+            num = float.Parse(textBox1.Text);
+            textBox1.Clear();
+            textBox1.Focus();
+            count = 2;
+            label1.Text = num.ToString() + "-";
+        }
+
+        private void Inmultire_Click(object sender, EventArgs e)
+        {
+            num = float.Parse(textBox1.Text);
+            textBox1.Clear();
+            textBox1.Focus();
+            count = 3;
+            label1.Text = num.ToString() + "*";
+        }
+
+        private void Impartire_Click(object sender, EventArgs e)
+        {
+            num = float.Parse(textBox1.Text);
+            textBox1.Clear();
+            textBox1.Focus();
+            count = 4;
+            label1.Text = num.ToString() + "/";
+        }
+
+        private void Egal_Click(object sender, EventArgs e)
+        {
+            compute();
+            label1.Text = "";
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int lenght = textBox1.TextLength - 1;
+            string text = textBox1.Text;
+            textBox1.Clear();
+            for (int i = 0; i < lenght; i++)
+                textBox1.Text = textBox1.Text + text[i];
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
